c646cbfc2455628f3cf07eabd363bf37f0e69e69
mfbxkjw7 <joshua.woodcock@manchester.ac.uk>
2019-06-25 14:17:43 +0100

    successfully update (for first page, need to sort looping over reviews)

--- rt_update.py
+++ rt_update.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup 
+from selenium import webdriver 
+
 import requests, sys, webbrowser, bs4
 import pandas as pd
 import openpyxl
@@ -6,48 +8,88 @@ import re
 
 # Go To User Review URL and load all the reviews
 
-
 date_list = []
 username_list = []
 rating_list = []
 text_list = []
 useful_list = []
 
-# Number of review pages
-# number_reviews = 129
-
-# pagenumber = 1
-
-# for page in range(number_reviews):
-
-url_sub = 'https://www.rottentomatoes.com/m/cache_2005/reviews?type=user'
+url_sub = 'https://www.rottentomatoes.com/m/cache_2005/reviews/?page=%s&type=user&sort='
 url = url_sub
 r = requests.get(url)
 bs = BeautifulSoup(r.text, 'html.parser')
 
-print(bs)
+# print(bs)
 
-# pagenumber += 1
+review_number = 0 
+total_page_number = 0
 
 for review in bs.findAll('li', {'class': 'audience-reviews__item'}):
+    
+    number_of_reviews = len(bs.findAll('li', {'class': 'audience-reviews__item'}))
 
-                # Find username
-                username = review.find("a", {"class": 'audience-reviews__name'})
-                username = username.text
-                
-                # find date
-                date = review.find("span", {"class": "audience-reviews__duration"}).contents
-                
-                # Find the text
-                review = review.find('p', attrs={'class': 'audience-reviews__review'})
-                text  = review.text
+    # Find username
+    username = review.find("a", {"class": 'audience-reviews__name'})
+    username = username.text
+    
+    # find date
+    date = review.find("span", {"class": "audience-reviews__duration"})
+    date = date.text 
+    
+    # Find the text
+    review_text = review.find('p', attrs={'class': 'audience-reviews__review'})
+    text  = review_text.text
 
-                # # find the rating 
-                # find_rating = review.find("div", {"class": "scoreWrapper" })
-                # string_rating = str(find_rating)
-                # rating = re.sub("[^0-9]", "", string_rating)
+    # find the rating 
+    find_rating_filled = review.findAll("span", {"class": "star-display__filled" })
+    find_rating_half = review.findAll("span", {"class": "star-display__half" })
 
-                # if any(str.isdigit(c) for c in rating) is False:
-                #         rating = 'Not Interested' 
-                # else:
-                #         rating = int(rating) / 10
+    full_star = len(review.findAll("span", {"class": "star-display__filled" }))
+    half_star = len(review.findAll("span", {"class": "star-display__half" }))
+
+    if half_star > 0:
+        half_star = 0.5
+    else:
+        half_star = 0
+
+    rating = full_star + half_star
+
+    # Is a superuser?
+
+    date_list.append(date)
+    username_list.append(username)
+    rating_list.append(rating)
+    text_list.append(text)
+
+    review_number += 1
+
+    if review_number == number_of_reviews:
+        
+        total_page_number += 1
+        
+        browser = webdriver.Firefox()
+        browser.get(url)
+        
+        for page in range(total_page_number):
+            button = browser.find_element_by_css_selector('nav.prev-next-paging__wrapper:nth-child(3) > button:nth-child(2) > span:nth-child(1)')
+            button.click()
+            browser.delete_all_cookies()
+            button = browser.find_element_by_css_selector('nav.prev-next-paging__wrapper:nth-child(3) > button:nth-child(2) > span:nth-child(1)')
+            button.click() 
+            
+
+        
+
+# Create dataframe
+review_data = pd.DataFrame(
+    {
+     'Date': date_list,
+     'Username': username_list,
+     'Rating (out of 5)': rating_list,
+     'Review Text': text_list,
+     }
+)
+
+review_data.sort_values(by=['Date'])
+
+review_data.to_csv('rt_user_reviews.csv')
