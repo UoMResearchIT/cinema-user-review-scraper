615148b4950c8d0c090c34969ec7e9bb9f62e78d
mfbxkjw7 <joshua.woodcock@manchester.ac.uk>
2019-08-02 15:14:50 +0100

    Rough version of imdb complete.

--- imdb.py
+++ imdb.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver 
 import requests, sys, webbrowser, bs4
 import pandas as pd
-import openpyxl
 
 # Go To User Review URL and load all the reviews
 
@@ -19,18 +18,18 @@ import openpyxl
 # next_button = browser.find_element_by_id('load-more-trigger')
 # type(next_button)
 
-review_count = 326
+# film_name = input("Enter the title of film: ")
+# url_input = input("Enter URL of User Reviews: ")
 
-# for a in range(review_count):
-    # next_button.click()
-    # time.sleep(2)
-
-
-url = 'https://www.imdb.com/title/tt0387898/reviews'
+# url_sub = url_input
+url = 'https://www.imdb.com/title/tt1602620/reviews?ref_=tt_ov_rt'
 r = requests.get(url)
-bs = BeautifulSoup(r.text, 'html.parser')
 
-print(bs)
+browser = webdriver.Firefox()
+browser.get(url)
+
+number_of_reviews = 1000
+
 
 title_list = []
 rating_list = []
@@ -39,29 +38,33 @@ date_list = []
 text_list = []
 useful_list = []
 
-for review in bs.findAll('div', {'class': 'review-container'}):
-    # rating = review.findAll('span', {'class' : 'rating-other-user-rating'}) 
-    # user_rating = rating.span[2].contents
-    title = review.a.contents
-    rating = review.findAll('span')[1].contents
-    username = review.findAll("a")[1].contents 
-    date = review.find("span", {"class": "review-date"}).contents
-    text = review.find("div", {"class": "text" }).contents
-    useful = review.select('div.actions.text-muted') 
 
-    title_list.append(title)
-    rating_list.append(rating)
-    username_list.append(username)
-    date_list.append(date)
-    text_list.append(text)
-    useful_list.append(useful)
+
+for page in range(number_of_reviews):
+    try:
+        button = browser.find_element_by_class_name('ipl-load-more__button')
+        button.click()
+    except:
+        print('Exception')
+
+bs = BeautifulSoup(browser.page_source, 'html.parser')
     
-    # print(title)
-    # print(rating)
-    # print(username)
-    # print(date)
-    # print(text)
-    # print(useful)
+for review in bs.findAll('div', {'class': 'review-container'}):
+        # rating = review.findAll('span', {'class' : 'rating-other-user-rating'}) 
+        # user_rating = rating.span[2].contents
+        title = review.a.contents
+        rating = review.findAll('span')[1].contents
+        username = review.findAll("a")[1].contents 
+        date = review.find("span", {"class": "review-date"}).contents
+        text = review.find("div", {"class": "text" }).contents
+        useful = review.select('div.actions.text-muted') 
+
+        title_list.append(title)
+        rating_list.append(rating)
+        username_list.append(username)
+        date_list.append(date)
+        text_list.append(text)
+        useful_list.append(useful)  
 
 # Create dataframe
 review_data = pd.DataFrame(
@@ -74,6 +77,5 @@ review_data = pd.DataFrame(
      }
 )
 
-review_data.to_csv('imdb.csv')
-review_data.to_excel('review_data.xlsx')
+review_data.to_csv('amour_imdb.csv')
 
